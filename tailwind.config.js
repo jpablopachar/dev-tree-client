19d9a8dce4a1f35a54906f3d9fde8b0f2923aed6
jpablopachar <jpablopachar1993@gmail.com>
2024-12-06 16:01:45 -0500

    Se instala paquetes del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "home" : "url('/bg.svg')"
+      },
+      backgroundSize: {
+        "home-xl" : "50%"
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
+
